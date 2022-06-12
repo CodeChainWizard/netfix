d40c5aff9414f1969c06084a2fdc3c950eda46ea
Shubham danecha <shubhamdanecha789@gmail.com>
2022-06-12 17:12:21 +0530

    ADD: Added all types of movies

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,12 @@ function App() {
         fetchUrl={requests.fetchNetflixOriginals}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="TopRated" fetchUrl={requests.fetchTopRated} />
+      <Row title="ActionMovies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="ComedyMovies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HorrorMovies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="RomanceMovies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
